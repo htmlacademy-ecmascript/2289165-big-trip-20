330f2f1cdaacaa2b1a78ba55d5bfea70ef5f701e
Semenets Ekaterina <cortes.lex@gmail.com>
2023-06-13 22:14:33 +0300

    feat: sorting by day(default), time and price.

--- src/presenter/trip-events-list-presenter.js
+++ src/presenter/trip-events-list-presenter.js
@@ -4,15 +4,21 @@ import SortingView from '../view/sort-view.js';
 import TripEventsListEmptyView from '../view/trip-events-list-empty-view.js';
 import TripPointPresenter from './trip-point-presenter.js';
 import { updateTripPoint } from '../utils.js';
+import { SortType } from '../const.js';
+import * as dayjs from 'dayjs';
 
 export default class TripEventsListPresenter {
   #tripEventsListComponent = new TripEventsListView();
-  #sortingComponent = new SortingView();
+
 
   #tripEventsListContainer = null;
   #tripPointsModel = null;
   #tripPoints = [];
   #tripPointPresenters = new Map();
+
+  #sortingComponent = null;
+  #currentSortType = SortType.DAY;
+  #defaultTripPoints = [];
 
   constructor({ tripEventsListContainer, tripPointsModel }) {
     this.#tripEventsListContainer = tripEventsListContainer;
@@ -20,7 +26,8 @@ export default class TripEventsListPresenter {
   }
 
   init() {
-    this.#tripPoints = [...this.#tripPointsModel.tripPoints];
+    this.#tripPoints = [...this.#tripPointsModel.tripPoints].sort((firstPoint, secondPoint) => firstPoint.dateFrom - secondPoint.dateFrom);
+    this.#defaultTripPoints = [...this.#tripPointsModel.tripPoints].sort((firstPoint, secondPoint) => firstPoint.dateFrom - secondPoint.dateFrom);
     this.#renderTripEventsList();
   }
 
@@ -37,7 +44,44 @@ export default class TripEventsListPresenter {
     render(new TripEventsListEmptyView(), this.#tripEventsListContainer);
   }
 
+  #sortPoints(sortType) {
+    switch (sortType) {
+      case SortType.TIME:
+        this.#tripPoints.sort((firstPoint, secondPoint) => {
+          const firstPointDuration = dayjs(firstPoint.dateTo).diff(dayjs(firstPoint.dateFrom));
+          const secondPointDuration = dayjs(secondPoint.dateTo).diff(dayjs(secondPoint.dateFrom));
+          return secondPointDuration - firstPointDuration;
+        });
+        break;
+      case SortType.PRICE:
+        this.#tripPoints.sort((firstPoint, secondPoint) => secondPoint.basePrice - firstPoint.basePrice);
+        break;
+      default:
+        this.#tripPoints = [...this.#defaultTripPoints];
+    }
+
+    this.#currentSortType = sortType;
+  }
+
+  #clearTripEventsList() {
+    this.#tripPointPresenters.forEach((presenter) => presenter.destroy());
+    this.#tripPointPresenters.clear();
+  }
+
+  #handleSortTypeChange = (sortType) => {
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+
+    this.#sortPoints(sortType);
+    this.#clearTripEventsList();
+    this.#renderTripPointsToList();
+  };
+
   #renderSorting() {
+    this.#sortingComponent = new SortingView({
+      onSortTypeChange: this.#handleSortTypeChange
+    });
     render(this.#sortingComponent, this.#tripEventsListComponent.element);
   }
 
@@ -64,6 +108,7 @@ export default class TripEventsListPresenter {
 
   #handleTripPointChange = (updatedData) => {
     this.#tripPoints = updateTripPoint(this.#tripPoints, updatedData);
+    this.#defaultTripPoints = updateTripPoint(this.#defaultTripPoints, updatedData);
     this.#tripPointPresenters.get(updatedData.id).init(updatedData);
   };
 }
