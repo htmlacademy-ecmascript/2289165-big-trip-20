d9e614ea71ef0e781e9686755644c73e9132a987
Semenets Ekaterina <cortes.lex@gmail.com>
2023-06-21 12:39:03 +0300

    fix: delete Amsterdam

--- src/view/trip-point-view.js
+++ src/view/trip-point-view.js
@@ -37,7 +37,7 @@ const createTripPointTemplate = (tripPoint, destinations, allOffers) => {
       <div class="event__type">
         <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">${type} ${destinationById.name} Amsterdam</h3>
+      <h3 class="event__title">${type} ${destinationById.name} </h3>
       <div class="event__schedule">
         <p class="event__time">
           <time class="event__start-time" datetime="2019-03-18T10:30">${timeFrom}</time>
